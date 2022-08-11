bb938f642ac923c7920b5835003960ea07417a1c
Davide Pollicino <davidepollicino2015@gmail.com>
2022-08-11 12:57:15 +0200

    Adds testing presente of the to-do item

--- to-do-list-react-v1/src/App.test.js
+++ to-do-list-react-v1/src/App.test.js
@@ -1,7 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import React from 'react';
-
+import user from '@testing-library/user-event';
 
 describe('Test form components rendering ', () => {
 
@@ -20,9 +19,21 @@ describe('Test form components rendering ', () => {
 });
 
 describe('Test todo-list creation', () => {
-
-
+  test('Todo item creation', () => {
+    // render the App component
+    render(<App />);
+    // add value inside the input field
+    user.type(getInputAddTodo(), 'My first to do');
+    // emulate click on button 
+    user.click(getAddTodobutton());
+    // check if the todo item is in the list
+    const todoItem = screen.getByText(/My first to do/i);
+    // check that todo item is in the list
+    expect(todoItem).toBeInTheDocument();
+  });
 });
+
+
 
 describe('Test todo-item update', () => {
 
@@ -33,5 +44,12 @@ describe('Test to-do item delete operation', () => {
 });
 
 
+// return the instance to the addTodo button
+function getAddTodobutton() {
+  return screen.getByRole('button', { name: /Add Todo/i })
+}
 
-
+// Return the input text to create a Todo item
+function getInputAddTodo() {
+  return screen.getByPlaceholderText(/Add a Todo/i)
+}
