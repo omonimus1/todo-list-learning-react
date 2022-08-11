330304357032756f9a7174af1a9e5762496ca706
Davide Pollicino <davidepollicino2015@gmail.com>
2022-08-11 12:41:29 +0200

    Adds scheleton for add to-do itema operation

--- to-do-list-react-v1/src/App.test.js
+++ to-do-list-react-v1/src/App.test.js
@@ -2,9 +2,12 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-describe('Testing form components rendering ', () => {
+
+describe('Test form components rendering ', () => {
+
+  // Mind that it and test are used for the same reasons as in the App.test.js file
   test("Test rendering of todo's form", () => {
-    render(<App />);
+    render(<App />); // must be rendered before testing (at any test)
     const linkElement = screen.getByText(/Plan for Today?/i);
     expect(linkElement).toBeInTheDocument();
   });
@@ -14,6 +17,18 @@ describe('Testing form components rendering ', () => {
     const addTodoButton = screen.getByRole('button', { name: /Add Todo/i })
     expect(addTodoButton).toBeInTheDocument();
   });
+});
+
+describe('Test todo-list creation', () => {
+
+
+});
+
+describe('Test todo-item update', () => {
+
+});
+
+describe('Test to-do item delete operation', () => {
 
 });
 
