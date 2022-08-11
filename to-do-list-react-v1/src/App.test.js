0c834a45ac683fd08ff4b863d483d4b8de33fdf0
Davide Pollicino <davidepollicino2015@gmail.com>
2022-08-11 12:38:46 +0200

    Improve structure Jtests

--- to-do-list-react-v1/src/App.test.js
+++ to-do-list-react-v1/src/App.test.js
@@ -2,19 +2,20 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 import React from 'react';
 
-test("Test rendering of todo's form and addTodo button", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Plan for Today?/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Testing form components rendering ', () => {
+  test("Test rendering of todo's form", () => {
+    render(<App />);
+    const linkElement = screen.getByText(/Plan for Today?/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("Test rendering of addTodo button", () => {
+    render(<App />);
+    const addTodoButton = screen.getByRole('button', { name: /Add Todo/i })
+    expect(addTodoButton).toBeInTheDocument();
+  });
+
 });
-
-
-test("Test rendering of addTodo button", () => {
-  render(<App />);
-  const addTodoButton = screen.getByRole('button', { name: /Add Todo/i })
-  expect(addTodoButton).toBeInTheDocument();
-});
-
 
 
 
